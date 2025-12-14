b5c708318598c5d0d05912f17c0471b367d20c68
Ostik <117195748+Ostik1220@users.noreply.github.com>
2025-12-14 18:39:41 +0200

    trying hooks

---
+++ README.old.md
@@ -0,0 +1,1 @@
+# countrt-hooks
---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <button onClick={() => setCount(count - 1)}>-</button>
+      {count}
+      <button onClick={() => setCount(count + 1)}>+</button>
+    </div>
+  );
+}
+
+export default App;
